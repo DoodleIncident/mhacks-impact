270cd0053daa1868fad03cf6252d70e8e31e79fd
Adam Johnson <aj71593@aol.com>
2012-11-02 22:54:19 -0400

    Added some class structure

---
+++ Android Workspace/src/Meal.java
@@ -0,0 +1,34 @@
+
+public class Meal 
+{
+	String name;
+	
+	String servingSize;
+	int portionSize; //size in grams
+
+	String[] allergens;
+	
+	//nutrition values
+	int calories;
+	int caloriesFromFat;
+	int totalFat;
+	int saturatedFat;
+	int transFat;
+	int cholesterol;
+	int sodium;
+	int totalCarbohydrate;
+	int dietaryFiber;
+	int sugars;
+	int protein;
+	int vitaminA;
+	int vitaminC;
+	int calcium;
+	int iron;
+	
+	//boolean flags
+	boolean vegan;
+	boolean vegetarian;
+	boolean msmart;
+	boolean glutenfree;
+
+}
---
+++ Android Workspace/src/MealSession.java
@@ -0,0 +1,9 @@
+
+
+
+public class MealSession 
+{
+	Meal[] availableMeals;
+	int time;
+	
+}
