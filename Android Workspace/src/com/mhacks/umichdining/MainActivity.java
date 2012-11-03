0455135431bfdaf1681254341fa11aca7c7dcc81
Charlie Lewis <noodle@noodle-Aspire-5830TG.(none)>
2012-11-02 23:23:32 -0400

    Eclipse workspace test

--- Android Workspace/src/com/mhacks/umichdining/MainActivity.java
+++ Android Workspace/src/com/mhacks/umichdining/MainActivity.java
@@ -1,7 +1,16 @@
 package com.mhacks.umichdining;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Xml;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,11 +19,68 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        XmlPullParser parser = Xml.newPullParser();
+        parser.setFeature
     }
+    
+    
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+    
+}
+
+public class menuParser {
+	private static final String ns = null;
+    
+    public List parse(InputStream in) throws XmlPullParserException, IOException {
+        try {
+            XmlPullParser parser = Xml.newPullParser();
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(in, null);
+            parser.nextTag();
+            return readFeed(parser);
+        } finally {
+            in.close();
+        }
+    }
+    
+    private List readFeed(XmlPullParser parser) throws XmlPullParserException, IOException {
+        List entries = new ArrayList();
+
+        parser.require(XmlPullParser.START_TAG, ns, "feed");
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            // Starts by looking for the entry tag
+            if (name.equals("entry")) {
+                entries.add(readEntry(parser));
+            } else {
+                skip(parser);
+            }
+        }  
+        return entries;
+    }
+    
+    private readMenuItem
+    
+    private readCourse
+    
+    private readStation
+    
+    private readName
+    
+    private readMeal
+    
+    private readDate
+    
+    private readMenu
+    
+    private readDiningHall
 }
